bbeddbf143ea0e2a3b2ed22d81c602df8de7658d
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2021-04-14 19:20:54 +0000

    hack solving sqlalchemy error with postgres

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 # database condiguration
 if os.environ.get("DATABASE_URL") is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
